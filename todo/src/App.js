a3b24988f7bbfb89a45a191badfa0f3c4e3f944a
Chirag <chirag.sanghvi@kevit.io>
2023-05-17 17:12:58 +0530

    first push

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import { useRef, useState } from "react"
 
 function App ()
 {
+  const [ text, setText ] = useState( '' )
+  const [ tasks, setTasks ] = useState( [] )
+  const inputref = useRef( {} )
   const savetodo = () =>
   {
-    alert()
+    if ( !text || !setText )
+    {
+      alert( "Please add a text" );
+      return;
+    }
+    setTasks( [ ...tasks, text ] );
+    setText( '' );
   }
   return (
     <div className="App">
       <h1>TODO</h1>
-      <input name="todo" />&nbsp;
+      <input placeholder="Task Name" onChange={ ( e ) => setText( e.target.value ) } value={ text } ref={ inputref } />&nbsp;
       <button onClick={ () => savetodo() }>Add Todo</button>
-
+      { tasks.map( ( stask, i ) => <h2 key={ i }>{ stask }</h2> ) }
     </div>
   );
 }

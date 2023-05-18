3f00bf8be13f0181fe20f2695634e8b29ef2a08f
Dharmik <dharmik.radadiya@kevit.io>
2023-05-18 15:19:54 +0530

    TodoList Component added

---
+++ dharmik/src/components/TodoList.tsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import TodoContext from "../store/store";
+import "./todo.css";
+
+const TodoList = () => {
+  const { todos, toggleTodo, deleteTodo } = useContext(TodoContext);
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            {todo.text}
+          </span>
+          <button onClick={() => toggleTodo(todo.id)}>
+            {todo.completed ? "Undo" : "Complete"}
+          </button>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;

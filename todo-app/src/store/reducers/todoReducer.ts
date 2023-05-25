17da23097a055f54800ef47228edac7cd134c485
Dharmik <dharmik.radadiya@kevit.io>
2023-05-25 19:08:33 +0530

    Created separated reducer file

---
+++ todo-app/src/store/reducers/todoReducer.ts
@@ -0,0 +1,41 @@
+export interface Todo {
+  todos: {
+    completed: boolean;
+    id: number;
+    text: string;
+  }[];
+  filter: string;
+}
+
+const todoReducer = (state: Todo, action: any) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case "SET_FILTER":
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
